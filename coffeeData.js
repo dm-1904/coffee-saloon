132b88f3a2355b8b317e052ab2602c0ff74ffcff
damonryon <damon.ryon@gmail.com>
2025-02-28 07:16:00 -0700

    coffeeData complete, adding bakeryData

--- coffeeData.js
+++ coffeeData.js
@@ -2,96 +2,194 @@ export const coffeeData = [
   [
     { catagory: "Brewed Coffee" },
     {
-      catagory: "Brewed Coffee",
+      category: "Brewed Coffee",
       coffeeName: "Featured Blonde - Single-Origin Ethiopia",
       calories: 5,
       image:
         "/assets/brewed-coffee/featured-blonde-single-origin-ethiopia.jpeg",
     },
     {
-      catagory: "Brewed Coffee",
+      category: "Brewed Coffee",
       coffeeName: "Featured Medium Roast - Pike Place Roast",
       calories: 5,
       image:
         "/assets/brewed-coffee/featured-medium-roast-pike-place-roast.jpeg",
     },
     {
-      catagory: "Brewed Coffee",
+      category: "Brewed Coffee",
       coffeeName: "Featured Dark Roast",
       calories: 5,
       image: "/assets/brewed-coffee/featured-dark-roast.jpeg",
     },
     {
-      catagory: "Brewed Coffee",
+      category: "Brewed Coffee",
       coffeeName: "Decaf Pike Place Roast",
       calories: 5,
       image: "/assets/brewed-coffee/decaf-pike-place-roast.jpeg",
     },
     {
-      catagory: "Brewed Coffee",
+      category: "Brewed Coffee",
       coffeeName: "Caffé Misto",
       calories: 110,
       image: "/assets/brewed-coffee/caffe-misto.png",
     },
   ],
   [
-    { catagory: "Cowboy" },
+    { category: "Cowboy" },
     {
-      catagory: "Cowboy",
+      category: "Cowboy",
       coffeeName: "American Cowboy",
       calories: 15,
       image: "/assets/cowboy-americano/american-cowboy.jpeg",
     },
   ],
   [
-    { catagory: "Latte" },
+    { category: "Latte" },
     {
-      catagory: "Latte",
+      category: "Latte",
       coffeeName: "Caffè Latte",
       calories: 190,
       image: "/assets/latte/caffe-latte.png",
     },
     {
-      catagory: "Latte",
+      category: "Latte",
       coffeeName: "Pistachio Latte",
       calories: 320,
       image: "/assets/latte/pistachio-latte.png",
     },
     {
-      catagory: "Latte",
+      category: "Latte",
       coffeeName: "Cinnamon Dolce Latte",
       calories: 340,
       image: "/assets/latte/cinnamon-dolce-latte.jpeg",
     },
     {
-      catagory: "Latte",
+      category: "Latte",
       coffeeName: "Blonde Vanilla Latte",
       calories: 250,
       image: "/assets/latte/blonde-vanilla-latte.png",
     },
     {
-      catagory: "Latte",
+      category: "Latte",
       coffeeName: "Caramel Brulèe Latte",
       calories: 410,
       image: "/assets/latte/carmel-brulee-latte.png",
     },
     {
-      catagory: "Latte",
+      category: "Latte",
       coffeeName: "Chestnut Praline Latte",
       calories: 330,
       image: "/assets/latte/chestnut-praline-latte.png",
     },
     {
-      catagory: "Latte",
+      category: "Latte",
       coffeeName: "Gingerbread Latte",
       calories: 310,
       image: "/assets/latte/gingerbread-latte.png",
     },
     {
-      catagory: "Latte",
+      category: "Latte",
       coffeeName: "Sugar Cookie Almondmilk Latte",
       calories: 180,
       image: "/assets/latte/sugar-cookie-almondmilk-latte.png",
     },
   ],
+  [
+    { catagory: "Cappuccino" },
+    {
+      catagory: "Cappuccino",
+      coffeeName: "Cappuccino",
+      calories: 140,
+      image: "/assets/cappuccino/cappuccino.jpeg",
+    },
+  ],
+  [
+    { catagory: "Mocha" },
+    {
+      catagory: "Mocha",
+      coffeeName: "Caffè Mocha",
+      calories: 370,
+      image: "/assets/mocha/caffe-mocha.png",
+    },
+    {
+      catagory: "Mocha",
+      coffeeName: "White Chocolate Mocha",
+      calories: 390,
+      image: "/assets/mocha/white-chocolate-mocha.png",
+    },
+    {
+      catagory: "Mocha",
+      coffeeName: "Peppermint Mocha",
+      calories: 440,
+      image: "/assets/mocha/peppermint-mocha.png",
+    },
+    {
+      catagory: "Mocha",
+      coffeeName: "Peppermint White Chocolate Mocha",
+      calories: 480,
+      image: "/assets/mocha/peppermint-white-chocolate-mocha.png",
+    },
+  ],
+  [
+    { catagory: "Macchiato" },
+    {
+      catagory: "Macchiato",
+      coffeeName: "Espresso Macchiato",
+      calories: 15,
+      image: "assets/macchiato/espresso-macchiato.png",
+    },
+    {
+      catagory: "Macchiato",
+      coffeeName: "Caramel Macchiato",
+      calories: 250,
+      image: "assets/macchiato/caramel-macchiato.png",
+    },
+  ],
+  [
+    { catagory: "Flat White" },
+    {
+      catagory: "Flat White",
+      coffeeName: "Flat White",
+      calories: 170,
+      image: "assets/flat-white/flat-white.png",
+    },
+    {
+      catagory: "Flat White",
+      coffeeName: "Honey Almondmilk Flat White",
+      calories: 120,
+      image: "assets/flat-white/honey-almondmilk-flat-white.png",
+    },
+  ],
+  [
+    { catagory: "Cortado" },
+    {
+      catagory: "Cortado",
+      coffeeName: "Cortado",
+      calories: 90,
+      image: "assets/cortado/cortado.png",
+    },
+    {
+      catagory: "Cortado",
+      coffeeName: "Brown Sugar Oatmilk Cortado",
+      calories: 130,
+      image: "assets/cortado/brown-sugar-oatmilk-cortado.png",
+    },
+  ],
+  [
+    { catagory: "Espresso Shot" },
+    {
+      catagory: "Espresso Shot",
+      coffeeName: "Espresso",
+      calories: 10,
+      image: "assets/espresso-shot/espresso.png",
+    },
+    {
+      catagory: "Espresso Shot",
+      coffeeName: "Espresso Con Panna",
+      calories: 35,
+      image: "assets/espresso-shot/espresso-con-panna.png",
+    },
+  ],
 ];
+
+export const bakeryData = [];
