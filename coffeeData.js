70118974d9ac2a50b1cc62881978ef1c9bdce880
damonryon <damon.ryon@gmail.com>
2025-02-27 20:58:52 -0700

    building coffeeData

---
+++ coffeeData.js
@@ -0,0 +1,97 @@
+export const coffeeData = [
+  [
+    { catagory: "Brewed Coffee" },
+    {
+      catagory: "Brewed Coffee",
+      coffeeName: "Featured Blonde - Single-Origin Ethiopia",
+      calories: 5,
+      image:
+        "/assets/brewed-coffee/featured-blonde-single-origin-ethiopia.jpeg",
+    },
+    {
+      catagory: "Brewed Coffee",
+      coffeeName: "Featured Medium Roast - Pike Place Roast",
+      calories: 5,
+      image:
+        "/assets/brewed-coffee/featured-medium-roast-pike-place-roast.jpeg",
+    },
+    {
+      catagory: "Brewed Coffee",
+      coffeeName: "Featured Dark Roast",
+      calories: 5,
+      image: "/assets/brewed-coffee/featured-dark-roast.jpeg",
+    },
+    {
+      catagory: "Brewed Coffee",
+      coffeeName: "Decaf Pike Place Roast",
+      calories: 5,
+      image: "/assets/brewed-coffee/decaf-pike-place-roast.jpeg",
+    },
+    {
+      catagory: "Brewed Coffee",
+      coffeeName: "Caffé Misto",
+      calories: 110,
+      image: "/assets/brewed-coffee/caffe-misto.png",
+    },
+  ],
+  [
+    { catagory: "Cowboy" },
+    {
+      catagory: "Cowboy",
+      coffeeName: "American Cowboy",
+      calories: 15,
+      image: "/assets/cowboy-americano/american-cowboy.jpeg",
+    },
+  ],
+  [
+    { catagory: "Latte" },
+    {
+      catagory: "Latte",
+      coffeeName: "Caffè Latte",
+      calories: 190,
+      image: "/assets/latte/caffe-latte.png",
+    },
+    {
+      catagory: "Latte",
+      coffeeName: "Pistachio Latte",
+      calories: 320,
+      image: "/assets/latte/pistachio-latte.png",
+    },
+    {
+      catagory: "Latte",
+      coffeeName: "Cinnamon Dolce Latte",
+      calories: 340,
+      image: "/assets/latte/cinnamon-dolce-latte.jpeg",
+    },
+    {
+      catagory: "Latte",
+      coffeeName: "Blonde Vanilla Latte",
+      calories: 250,
+      image: "/assets/latte/blonde-vanilla-latte.png",
+    },
+    {
+      catagory: "Latte",
+      coffeeName: "Caramel Brulèe Latte",
+      calories: 410,
+      image: "/assets/latte/carmel-brulee-latte.png",
+    },
+    {
+      catagory: "Latte",
+      coffeeName: "Chestnut Praline Latte",
+      calories: 330,
+      image: "/assets/latte/chestnut-praline-latte.png",
+    },
+    {
+      catagory: "Latte",
+      coffeeName: "Gingerbread Latte",
+      calories: 310,
+      image: "/assets/latte/gingerbread-latte.png",
+    },
+    {
+      catagory: "Latte",
+      coffeeName: "Sugar Cookie Almondmilk Latte",
+      calories: 180,
+      image: "/assets/latte/sugar-cookie-almondmilk-latte.png",
+    },
+  ],
+];
